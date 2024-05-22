08712bff22c44798a399e3fc851c3900c24984fd
Adarsh Sahu <adarshsahu2510@gmail.com>
2024-05-22 16:59:07 +0530

    .

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,6 +9,9 @@ const sendEmail = async (options) => {
         clientId:
           "257899612719-9jsfnkbb5i5kgp6r34754vdlmjcdi6jb.apps.googleusercontent.com",
         clientSecret: "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",
+        refreshToken: "1//0g5ONv2b7i1amCgYIARAAGBASNwF-L9IrpT3FrnTdQd-uEW0ndlnKEYMTMrILiqpKPe6-wi4EMHbvsTCqQBvP_nfOYaUAEpF60Zc",
+        accessToken: "ya29.a0AXooCguRU1PAtWQ-w6kq3Y3tnNahwftsDnyRyH8zD1-IhNG1y-sBqwLmrSH_Ztzf8KwyfM1pr6U85WMZJXxFMX-5ZgYDjxGHC9OLKQPJciAInjNRQKlWiNCRNw4yhnbKXiwpba4QrPoOUAW-y5AkNiwWRdQx6vhgCAyGaCgYKAR0SARMSFQHGX2Milcwj1-L1JKfmbRwg6d_CNQ0171",
+
       },
     });
     transporter.on("token", (token) => {
@@ -24,14 +27,14 @@ const sendEmail = async (options) => {
       subject: options.subject, // Subject line
       html: options.html, // html body
 
-      auth: {
-        user: "adarshsahu2510@gmail.com",
-        refreshToken:
-          "1//0g5ONv2b7i1amCgYIARAAGBASNwF-L9IrpT3FrnTdQd-uEW0ndlnKEYMTMrILiqpKPe6-wi4EMHbvsTCqQBvP_nfOYaUAEpF60Zc",
-        accessToken:
-          "ya29.a0AXooCguRU1PAtWQ-w6kq3Y3tnNahwftsDnyRyH8zD1-IhNG1y-sBqwLmrSH_Ztzf8KwyfM1pr6U85WMZJXxFMX-5ZgYDjxGHC9OLKQPJciAInjNRQKlWiNCRNw4yhnbKXiwpba4QrPoOUAW-y5AkNiwWRdQx6vhgCAyGaCgYKAR0SARMSFQHGX2Milcwj1-L1JKfmbRwg6d_CNQ0171",
-        expires: 1494388182480,
-      },
+      // auth: {
+      //   user: "adarshsahu2510@gmail.com",
+      //   refreshToken:
+      //     "1//0g5ONv2b7i1amCgYIARAAGBASNwF-L9IrpT3FrnTdQd-uEW0ndlnKEYMTMrILiqpKPe6-wi4EMHbvsTCqQBvP_nfOYaUAEpF60Zc",
+      //   accessToken:
+      //     "ya29.a0AXooCguRU1PAtWQ-w6kq3Y3tnNahwftsDnyRyH8zD1-IhNG1y-sBqwLmrSH_Ztzf8KwyfM1pr6U85WMZJXxFMX-5ZgYDjxGHC9OLKQPJciAInjNRQKlWiNCRNw4yhnbKXiwpba4QrPoOUAW-y5AkNiwWRdQx6vhgCAyGaCgYKAR0SARMSFQHGX2Milcwj1-L1JKfmbRwg6d_CNQ0171",
+      //   expires: 1494388182480,
+      // },
     };
 
     //  transporter.sendMail(message);
