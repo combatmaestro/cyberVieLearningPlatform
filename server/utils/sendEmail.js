6fda381c82dfba0bdf849a4a78326e28b47dc9ed
Adarsh Sahu <adarshsahu2510@gmail.com>
2024-05-23 00:36:01 +0530

    mailing code

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,13 +5,14 @@ const sendEmail = async (options) => {
     let transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        type: "OAuth2",
-        clientId:
-          "257899612719-9jsfnkbb5i5kgp6r34754vdlmjcdi6jb.apps.googleusercontent.com",
-        clientSecret: "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",
-        refreshToken: "1//0g5ONv2b7i1amCgYIARAAGBASNwF-L9IrpT3FrnTdQd-uEW0ndlnKEYMTMrILiqpKPe6-wi4EMHbvsTCqQBvP_nfOYaUAEpF60Zc",
-        accessToken: "ya29.a0AXooCguRU1PAtWQ-w6kq3Y3tnNahwftsDnyRyH8zD1-IhNG1y-sBqwLmrSH_Ztzf8KwyfM1pr6U85WMZJXxFMX-5ZgYDjxGHC9OLKQPJciAInjNRQKlWiNCRNw4yhnbKXiwpba4QrPoOUAW-y5AkNiwWRdQx6vhgCAyGaCgYKAR0SARMSFQHGX2Milcwj1-L1JKfmbRwg6d_CNQ0171",
-
+        // type: "OAuth2",
+        // clientId:
+        //   "257899612719-9jsfnkbb5i5kgp6r34754vdlmjcdi6jb.apps.googleusercontent.com",
+        // clientSecret: "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",
+        // refreshToken: "1//0g5ONv2b7i1amCgYIARAAGBASNwF-L9IrpT3FrnTdQd-uEW0ndlnKEYMTMrILiqpKPe6-wi4EMHbvsTCqQBvP_nfOYaUAEpF60Zc",
+        // accessToken: "ya29.a0AXooCguRU1PAtWQ-w6kq3Y3tnNahwftsDnyRyH8zD1-IhNG1y-sBqwLmrSH_Ztzf8KwyfM1pr6U85WMZJXxFMX-5ZgYDjxGHC9OLKQPJciAInjNRQKlWiNCRNw4yhnbKXiwpba4QrPoOUAW-y5AkNiwWRdQx6vhgCAyGaCgYKAR0SARMSFQHGX2Milcwj1-L1JKfmbRwg6d_CNQ0171",
+        user:"adarshsahu2510@gmail.com",
+        pass:"bdws cnno opgb acoz"
       },
     });
     transporter.on("token", (token) => {
