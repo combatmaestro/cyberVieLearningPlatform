c731c341b6ff54b6ce2513eacc82abfca04511f8
Adarsh Sahu <adarshsahu2510@gmail.com>
2024-06-04 10:44:08 +0530

    .

--- server/controllers/ModuleController.js
+++ server/controllers/ModuleController.js
@@ -29,7 +29,7 @@ module.exports.getDetails = catchAsyncErrors(async (req, res, next) => {
     },
   })
 
-  console.log("user",req.user)
+ 
 
   if (!module) {
     return next(new ErrorHandler('Module not found', 404))
