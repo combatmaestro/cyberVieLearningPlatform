9761cb895d870ef8613428e0444edf2450068720
Adarsh Sahu <adarshsahu2510@gmail.com>
2024-06-24 03:33:22 +0530

    moduleImg added

--- client/src/components/ModulesList/ModulesList.js
+++ client/src/components/ModulesList/ModulesList.js
@@ -6,7 +6,9 @@ import { getAllModules } from "../../actions/moduleAction";
 import ModuleListLoader from "./ModuleListLoader";
 import LockIcon from "@material-ui/icons/Lock";
 import { useStyles } from "./style";
-
+import moduleImg from "./Cybersecurity_Training.png";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
 function ModulesList() {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -15,7 +17,7 @@ function ModulesList() {
   const user = userData.data;
   const { loading, data = [], error } = modules;
   const history = useHistory();
-  
+
   useEffect(() => {
     dispatch(getAllModules(user));
     console.log(data);
@@ -32,59 +34,91 @@ function ModulesList() {
   };
 
   // Sort modules based on the extracted number from description
-  const sortedModules = data.slice().sort((a, b) => getNumberFromDescription(a.description) - getNumberFromDescription(b.description));
+  const sortedModules = data
+    .slice()
+    .sort(
+      (a, b) =>
+        getNumberFromDescription(a.description) -
+        getNumberFromDescription(b.description)
+    );
 
   return (
     <div className={classes.root}>
-      {sortedModules.length > 0 ? sortedModules.map((module) => {
-        return (
-          <React.Fragment key={module._id}>
-            {!module.hidden && (
-              <div
-                className={classes.module}
-                style={{
-                  background:
-                    user.role === "user" &&
-                    user.tier === "free" &&
-                    module.type === "paid"
-                      ? "linear-gradient(133.06deg, rgba(37, 89, 131, 0.37) 9.35%, rgba(118, 194, 255, 0) 105.63%)"
-                      : "#FFFFFF",
-                  pointerEvents:
-                    user.role === "user" &&
-                    user.tier === "free" &&
-                    module.type === "paid"
-                      ? "none"
-                      : "",
-                }}
-              >
-                <div className={classes.titleBold}>{module.title}</div>
-                {user.role === "user" &&
-                user.tier === "free" &&
-                module.type === "paid" && (
-                  <div className={classes.lockIcon}>
-                    <LockIcon />
+      {sortedModules.length > 0
+        ? sortedModules.map((module) => {
+            return (
+              <React.Fragment key={module._id}>
+                {!module.hidden && (
+                  <div
+                    className={classes.module}
+                    style={{
+                      background:
+                        user.role === "user" &&
+                        user.tier === "free" &&
+                        module.type === "paid"
+                          ? "linear-gradient(133.06deg, rgba(37, 89, 131, 0.37) 9.35%, rgba(118, 194, 255, 0) 105.63%)"
+                          : "#FFFFFF",
+                      pointerEvents:
+                        user.role === "user" &&
+                        user.tier === "free" &&
+                        module.type === "paid"
+                          ? "none"
+                          : "",
+                    }}
+                  >
+                    <Grid container alignItems="center" spacing={2}>
+                      <Grid item xs={12} md={10}>
+                        <div className={classes.titleBold}>{module.title}</div>
+                      </Grid>
+                      <Grid item xs={12} md={2}>
+                        <img
+                          src={moduleImg}
+                          style={{ width: 50, height: 50, marginRight: 10 }}
+                        />
+                      </Grid>
+                    </Grid>
+                    {user.role === "user" &&
+                      user.tier === "free" &&
+                      module.type === "paid" && (
+                        <div className={classes.lockIcon}>
+                          <LockIcon />
+                        </div>
+                      )}
+                    <div className={classes.description}>
+                      {module.description}
+                    </div>
+                    <div className={classes.button}>
+                      {user.role === "user" &&
+                      user.tier === "free" &&
+                      module.type === "paid" ? (
+                        <Button
+                          variant="outlined"
+                          style={{
+                            background:
+                              " linear-gradient(298.54deg, #0A767B -7.7%, #00A7D6 97.12%)",
+                          }}
+                        >
+                          Premium
+                        </Button>
+                      ) : (
+                        <Button
+                          variant="outlined"
+                          onClick={() => clickHandler(module._id)}
+                          style={{
+                            background:
+                              " linear-gradient(298.54deg, #0A767B -7.7%, #00A7D6 97.12%)",
+                          }}
+                        >
+                          View Course
+                        </Button>
+                      )}
+                    </div>
                   </div>
                 )}
-                <div className={classes.description}>{module.description}</div>
-                <div className={classes.button}>
-                  {user.role === "user" &&
-                  user.tier === "free" &&
-                  module.type === "paid" ? (
-                    <Button variant="outlined">Premium</Button>
-                  ) : (
-                    <Button
-                      variant="outlined"
-                      onClick={() => clickHandler(module._id)}
-                    >
-                      View Course
-                    </Button>
-                  )}
-                </div>
-              </div>
-            )}
-          </React.Fragment>
-        );
-      }) : ""}
+              </React.Fragment>
+            );
+          })
+        : ""}
     </div>
   );
 }
