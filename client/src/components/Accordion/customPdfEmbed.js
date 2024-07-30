38425ef14ce5ed155256eef122bfa7464736a629
adarsh <adarshsahu2510@gmail.com>
2024-07-31 01:39:08 +0530

    .

--- client/src/components/Accordion/customPdfEmbed.js
+++ client/src/components/Accordion/customPdfEmbed.js
@@ -4,32 +4,32 @@ import React from 'react';
 
 const CustomPDFViewer = ({ file }) => {
 
-  const pdfData = `data:application/pdf;base64,${file}`;
-
-  return (
-    <iframe
-      src={pdfData}
-      title="PDF Viewer"
-      style={{ width: '100%', height: '700px', border: 'none' }}
-    ></iframe>
-  );
-  // const pdfData = atob(file);
-
-  // const pdfDataArray = new Uint8Array(pdfData.length);
-  // for (let i = 0; i < pdfData.length; i++) {
-  //   pdfDataArray[i] = pdfData.charCodeAt(i);
-  // }
-
-  // const pdfBlob = new Blob([pdfDataArray], { type: 'application/pdf' });
-  // const pdfUrl = URL.createObjectURL(pdfBlob);
+  // const pdfData = `data:application/pdf;base64,${file}`;
 
   // return (
-  //   <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
-  //     <div style={{ height: '500px' }}>
-  //       <Viewer fileUrl={pdfUrl} />
-  //     </div>
-  //   </Worker>
+  //   <iframe
+  //     src={pdfData}
+  //     title="PDF Viewer"
+  //     style={{ width: '100%', height: '700px', border: 'none' }}
+  //   ></iframe>
   // );
+  const pdfData = atob(file);
+
+  const pdfDataArray = new Uint8Array(pdfData.length);
+  for (let i = 0; i < pdfData.length; i++) {
+    pdfDataArray[i] = pdfData.charCodeAt(i);
+  }
+
+  const pdfBlob = new Blob([pdfDataArray], { type: 'application/pdf' });
+  const pdfUrl = URL.createObjectURL(pdfBlob);
+
+  return (
+    <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
+      <div style={{ height: '500px' }}>
+        <Viewer fileUrl={pdfUrl} />
+      </div>
+    </Worker>
+  );
 };
 
 export default CustomPDFViewer;
