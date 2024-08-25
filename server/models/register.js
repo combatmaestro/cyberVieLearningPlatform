9a3aa34e03bdddc4003aaf810817eeb12fa635a4
adarsh <adarshsahu2510@gmail.com>
2024-08-26 03:11:09 +0530

    .

--- server/models/register.js
+++ server/models/register.js
@@ -15,8 +15,8 @@ const formDataSchema = new mongoose.Schema({
   phoneNumber: {
     type: String,
     required: true,
+    unique: true,  // Ensure phoneNumber is unique
     trim: true,
-    unique: true,
   },
   organization: {
     type: String,
