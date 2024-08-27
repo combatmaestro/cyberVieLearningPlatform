72972e13f64283d3bbd32efc4e13e6a7eee5c9ba
adarsh <adarshsahu2510@gmail.com>
2024-08-27 23:11:50 +0530

    sending mail on leads recieved

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,5 +1,6 @@
 const FormData = require('../models/register');
 const portalUsers = require('../models/User');
+const sendEmail = require("../utils/sendEmail");
 exports.saveFormData = async (req, res) => {
   try {
     const { name, email, phoneNumber, organization, designation, experience, location, tier } = req.body;
@@ -7,10 +8,13 @@ exports.saveFormData = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: 'Email already exists' });
     }
-
-    
     const newFormData = await FormData.create(req.body);
-
+    await sendEmail({
+      //sending admin email
+      email: "info@cybervie.com",
+      html: `${name} with email ${email} and phone${phoneNumber} has joined the portal today`,
+      subject: "New Lead CSEP",
+    });
     return res.status(200).json({ message: 'Form data saved successfully', data: newFormData });
   } catch (error) {
     return res.status(500).json({ message: 'Error saving form data', error: error.message });
