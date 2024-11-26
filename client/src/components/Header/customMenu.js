000346fb2b3f3ac0c31ab675935d2e791d879004
adarsh <adarshsahu2510@gmail.com>
2024-11-26 14:37:26 +0530

    .

--- client/src/components/Header/customMenu.js
+++ client/src/components/Header/customMenu.js
@@ -6,7 +6,8 @@ import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { withStyles } from "@material-ui/core/styles";
 import { Fade, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux'
+import { userRefresh } from "../../actions/userActions";
 import { Link } from "react-router-dom";
 
 const StyledMenu = withStyles({
@@ -84,9 +85,17 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function CustomMenu(props) {
+  const dispatch = useDispatch()
   const { anchorEl, handleClose, data, signOutHandler } = props;
   const user = useSelector((state) => state.user);
   const classes = useStyles();
+
+
+  useEffect(() => {
+    //to refresh user
+    dispatch(userRefresh())
+  }, [dispatch])
+
 
   return (
     <div>
