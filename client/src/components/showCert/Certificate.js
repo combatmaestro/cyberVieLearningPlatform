862e4e8287a85ac681f31a8d496bfbeee444d105
adarsh <adarshsahu2510@gmail.com>
2024-11-26 23:54:32 +0530

    .

--- client/src/components/showCert/Certificate.js
+++ client/src/components/showCert/Certificate.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react';
-
+import { useSelector } from 'react-redux';
 const Certificate = () => {
   const [htmlContent, setHtmlContent] = useState('');
-
+  const user = useSelector((state) => state.user);
   useEffect(() => {
     fetch('/certificate.html')
       .then((response) => response.text())
-      .then((html) => setHtmlContent(html))
+      .then((html) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const date = new Date(user?.certificateDate);
+        const formattedDate = date.toISOString().split('T')[0];
+        // Example: Update fields using their IDs
+        doc.getElementById('studentName')?.textContent = user?.name;
+        doc.getElementById('referenceId')?.textContent = user?.certificateRefId;
+        doc.getElementById('dateOfIssue')?.textContent = formattedDate;
+
+        // Serialize the updated HTML back to a string
+        const updatedHtml = doc.documentElement.innerHTML;
+
+        setHtmlContent(updatedHtml);
+      })
       .catch((err) => console.error('Error loading HTML:', err));
   }, []);
 
   return (
-    <div className="certificate-container" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    <div className="certificate-container" style={{
+      marginLeft: '20%',
+      width: '60%',
+      padding: '1%',
+      border: '2px solid gray',
+      marginTop: '1%'
+    }}  dangerouslySetInnerHTML={{ __html: htmlContent }} />
   );
 };
 
