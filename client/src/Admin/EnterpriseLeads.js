d39575999720428f41f2656cc52b8a13b14742ea
Adarsh Sahu <adarshsahu2510@gmail.com>
2025-10-15 23:10:52 +0530

    .

--- client/src/Admin/EnterpriseLeads.js
+++ client/src/Admin/EnterpriseLeads.js
@@ -3,7 +3,7 @@ import { MDBDataTable } from "mdbreact";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllEnterpriseLeads } from "../actions/enterpriseLeadAction";
 import Loader from "../components/Loader/Loader";
-import SideDrawer from "../Drawer/SideDrawer";
+import SideDrawer from "./Drawer/SideDrawer";
 import { Grid, Box, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
