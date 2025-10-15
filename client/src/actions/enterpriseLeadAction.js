f1b38c5bb57f110d47a415d534b3ad08b9be65b9
Adarsh Sahu <adarshsahu2510@gmail.com>
2025-10-15 23:17:21 +0530

    .

--- client/src/actions/enterpriseLeadAction.js
+++ client/src/actions/enterpriseLeadAction.js
@@ -1,21 +1,23 @@
 import axios from "axios";
-import { ENTERPRISE_LEADS_REQUEST, ENTERPRISE_LEADS_SUCCESS, ENTERPRISE_LEADS_FAIL } from "../constants/enterpriseLeadConstants";
+import { GET_ENTERPRISE_LEADS_REQUEST,
+  GET_ENTERPRISE_LEADS_SUCCESS,
+  GET_ENTERPRISE_LEADS_FAILURE, } from "../constants/enterpriseLeadConstants";
 
 export const getAllEnterpriseLeads = () => async (dispatch) => {
   try {
-    dispatch({ type: ENTERPRISE_LEADS_REQUEST });
+    dispatch({ type: GET_ENTERPRISE_LEADS_REQUEST });
 
     const { data } = await axios.get(
       "https://cyber-vie-learning-platform-client-ten.vercel.app/user/admin/get-enterprise-leads"
     );
 
     dispatch({
-      type: ENTERPRISE_LEADS_SUCCESS,
+      type: GET_ENTERPRISE_LEADS_SUCCESS,
       payload: data.leads,
     });
   } catch (error) {
     dispatch({
-      type: ENTERPRISE_LEADS_FAIL,
+      type: GET_ENTERPRISE_LEADS_FAILURE,
       payload: error.response?.data?.message || error.message,
     });
   }
